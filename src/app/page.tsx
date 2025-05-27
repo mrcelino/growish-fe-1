ea59d6ffb59da8b55571ab21b600f7559785dc41
Marcelino <marcelinokenny12@gmail.com>
2025-05-27 21:01:32 +0700

    Responsive auth register login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,21 +52,21 @@ export default function Login() {
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center gap-4 bg-[#E7F5E5] h-fit xl:p-12 rounded-[30px] xl:w-1/3">
+      <div className="flex flex-col items-center justify-center gap-4 bg-[#E7F5E5] min-h-[350px] md:h-fit p-3 md:p-6 xl:p-12 rounded-xl md:rounded-[30px] w-full max-w-[355px] md:max-w-sm lg:max-w-full lg:w-1/3">
         <div className="flex flex-col items-center gap-2">
-          <h2 className="text-[#326D2C] text-2xl font-semibold">Login</h2>
-          <h2 className="font-medium text-xl">Selamat datang di Labora</h2>
+          <h2 className="text-[#326D2C] text-lg md:text-2xl font-semibold">Login</h2>
+          <h2 className="font-medium text-base md:text-xl">Selamat datang di Labora</h2>
         </div>
         {error && <p className="text-red-500 text-lg font-medium">{error}</p>}
         <form
           onSubmit={handleSubmit}
           className="w-full gap-4 flex flex-col items-center"
         >
-          <div className="flex flex-col gap-6 mt-4 w-full">
+          <div className="flex flex-col gap-6 mt-2 md:mt-4 w-5/6 md:w-full">
             <input
               type="email"
               placeholder="Email"
-              className="bg-white text-black placeholder-black font-medium rounded-2xl py-2 px-4"
+              className="bg-white text-black placeholder-black font-medium rounded-xl md:rounded-2xl py-2 px-4 text-sm md:text-base"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -74,21 +74,22 @@ export default function Login() {
             <input
               type="password"
               placeholder="Kata Sandi"
-              className="bg-white text-black placeholder-black font-medium rounded-2xl py-2 px-4"
+              className="bg-white text-black placeholder-black font-medium rounded-xl md:rounded-2xl py-2 px-4 text-sm md:text-base"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <Link href="/register" className="font-semibold text-sm">Belum punya akun?</Link>
+            <Link href="/register" className="font-semibold text-xs md:text-sm ml-2">Belum punya akun?</Link>
             <button
               type="submit"
-              className="flex space-x-2 items-center justify-center bg-[#80C978] font-semibold rounded-2xl p-2 cursor-pointer"
+              className="flex space-x-2 items-center justify-center bg-[#80C978] text-sm md:text-base font-semibold rounded-xl md:rounded-2xl p-2 cursor-pointer"
             >
               <Image
                 src="/upload.png"
                 alt="upload icon"
                 width={20}
                 height={20}
+                className="size-4"
               />
               <h2>Masuk</h2>
             </button>
